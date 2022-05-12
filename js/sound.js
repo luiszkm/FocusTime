c54dea8f39d83bf37a5145280a642421826c39f5
Luis Murilo Soares Pereira <95939288+luiszkm@users.noreply.github.com>
2022-05-12 09:58:27 -0300

    Update sound.js
    colocando a const springMusic para fora da factory

--- js/sound.js
+++ js/sound.js
@@ -1,26 +1,23 @@
 import { CardElements } from "./cardElements.js"
 const cardElements = CardElements()
-export function Sound (){
 
-  const springMusic = new Audio("https://github.com/ELLDev/timer-with-dark-mode/blob/main/sounds/Chuva.wav?raw=true")
+ const springMusic = new Audio("https://github.com/ELLDev/timer-with-dark-mode/blob/main/sounds/Chuva.wav?raw=true")
+ 
+export function Sound (){
 
   const playMusic = () =>{
     springMusic.play()
   }
-  // let rageInput = cardElements.volume.oninput = function(){
-  // // console.log(this.value);
   
-  //   return 
-  // }
   const musicVolume = (volumeValue) => {
   //  Number(volumeValue)
-    springMusic.volume = 0
+    springMusic.volume = Number(volumeValue)
     console.log(volumeValue);
     console.log(springMusic.volume);
   }
+  
   return{
     playMusic,
     musicVolume,
-    // rageInput
   }
 }
