6b7a4be9e4296be8bfd32cb7a129b9620c74795d
Luis Murilo Soares Pereira <luiszkm@gmail.com>
2022-05-14 21:32:00 -0300

    add new sound functions

--- js/index.js
+++ js/index.js
@@ -3,11 +3,14 @@ import { Controls } from "./timeControls.js"
 import { CardElements } from "./cardElements.js"
 import { CardControls } from "./cardControls.js"
 import { Sound } from "./sound.js"
+import { Modes } from "./modes.js"
+
 const sound = Sound()
 const cardControls = CardControls()
 const cardElements = CardElements()
 const elements = Elements()
 const controls = Controls()
+const modes = Modes()
 
 //Eventos »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»
 
@@ -21,6 +24,12 @@ cardElements.cardStorm.addEventListener('click', cardControls.storm)
 cardElements.cardWinter.addEventListener('click', cardControls.winter)
 cardElements.cardSummer.addEventListener('click', cardControls.summer)
 
-cardElements.volume.addEventListener('input',() =>{sound.musicVolume(cardElements.volume.value)
-  })
-
+/*Regulador de Voluems »»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»»» */
+cardElements.volumeStorm.addEventListener('input',() =>{sound.musicVolume(cardElements.volumeStorm.value)
+})
+cardElements.volume.addEventListener('input', () =>{sound.volumeStorm(cardElements.volume.value)
+})
+cardElements.volumeSummer.addEventListener('input',() =>{sound.volumeSummer(cardElements.volumeSummer.value)
+})
+cardElements.volumeWinter.addEventListener('input',() =>{sound.volumeWinter(cardElements.volumeWinter.value)
+})
